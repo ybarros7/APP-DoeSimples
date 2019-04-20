5b10b85e9b1b64167ea5385f8142ea5587d0c5ec
Giuseppe Rosa <giusepper11@gmail.com>
2019-04-20 11:39:27 -0300

    Adicionando filtragem de instituicao

---
+++ app/src/main/java/br/com/pipasdevteam/doesimples/FilterInstituicao.kt
@@ -0,0 +1,12 @@
+package br.com.pipasdevteam.doesimples
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FilterInstituicao : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filter_instituicao)
+    }
+}
